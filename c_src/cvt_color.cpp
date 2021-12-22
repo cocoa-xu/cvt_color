96d3c04a191775de85cb801c3915b53073cdec1a
Cocoa <89497197+cocoa-xu@users.noreply.github.com>
2021-12-22 22:18:49 +0000

    implemented `cvt_color_888_to_565`

--- c_src/cvt_color.cpp
+++ c_src/cvt_color.cpp
@@ -1,2 +1,77 @@
 #include "cvt_color.hpp"
+#include <memory>
+#include <vector>
+#include <string>
 #include "nif_utils.hpp"
+#include <erl_nif.h>
+
+#ifdef __GNUC__
+#  pragma GCC diagnostic ignored "-Wunused-parameter"
+#  pragma GCC diagnostic ignored "-Wmissing-field-initializers"
+#  pragma GCC diagnostic ignored "-Wunused-variable"
+#  pragma GCC diagnostic ignored "-Wunused-function"
+#endif
+
+static ERL_NIF_TERM cvt_color_nif(ErlNifEnv *env, int argc, const ERL_NIF_TERM argv[]) {
+    if (argc != 3) {
+        return erlang::nif::error(env, "expecting 3 arguments: image_data, channel_order_source, channel_order_target");
+    }
+
+    std::string source_color, target_color;
+    erlang::nif::get_atom(env, argv[1], source_color);
+    erlang::nif::get_atom(env, argv[2], target_color);
+    ErlNifBinary binary;
+    if (enif_inspect_binary(env, argv[0], &binary)) {
+        if (source_color == "rgb888" || source_color == "bgr888") {
+            if (binary.size % 24 == 0) {
+                ErlNifBinary result;
+                color src = color::RGB888, dst = color::RGB565;
+                if (source_color == "bgr888") src = color::BGR888;
+                if (target_color == "bgr565") src = color::BGR565;
+                uint8_t * out_data = nullptr;
+                size_t out_size = 0;
+                int ret = cvt_color(binary.data, binary.size, src, dst, &out_data, &out_size, [&result](size_t bytes) {
+                    enif_alloc_binary(bytes, &result);
+                    return result.data;
+                });
+                if (ret != 0) {
+                    return erlang::nif::error(env, "cvt failed");
+                } else {
+                    return enif_make_binary(env, &result);
+                }
+            }
+            return erlang::nif::error(env, "malformed BGR888/RGB888 binary data");
+        } else {
+            return erlang::nif::error(env, "not implemented yet");
+        }
+    } else {
+        return erlang::nif::error(env, "expecting bitstring");
+    }
+}
+
+static int on_load(ErlNifEnv* env, void**, ERL_NIF_TERM)
+{
+    return 0;
+}
+
+static int on_reload(ErlNifEnv*, void**, ERL_NIF_TERM)
+{
+    return 0;
+}
+
+static int on_upgrade(ErlNifEnv*, void**, void**, ERL_NIF_TERM)
+{
+    return 0;
+}
+
+#define F(NAME, ARITY) {#NAME, ARITY, NAME, 0}
+
+static ErlNifFunc nif_functions[] = {
+    {"cvt_color", 3, cvt_color_nif, 0}
+};
+
+ERL_NIF_INIT(cvt_color_nif, nif_functions, on_load, on_reload, on_upgrade, NULL);
+
+#if defined(__GNUC__)
+#pragma GCC visibility push(default)
+#endif
--- c_src/cvt_color.hpp
+++ c_src/cvt_color.hpp
@@ -2,5 +2,108 @@
 #define __CVT_COLOR_HPP__
 
 #pragma once
+#include <stdint.h>
+#include <functional>
+
+typedef enum {
+    RGB888,     // 24bit
+    RGB565,     // 16bit
+    BGR888,     // 24bit
+    BGR565      // 16bit
+} color;
+
+static int cvt_color(
+    const uint8_t * data,
+    size_t size,
+    color src,
+    color dst,
+    uint8_t ** out_data,
+    size_t * out_size,
+    const std::function<void*(size_t)>& alloc_func
+);
+
+static int cvt_color_888_to_565(
+    const uint8_t * data,
+    size_t size,
+    color src,
+    color dst,
+    uint8_t ** out_data,
+    size_t * out_size,
+    const std::function<void*(size_t)>& alloc_func
+);
+
+int cvt_color(
+    const uint8_t * data,
+    size_t size,
+    color src,
+    color dst,
+    uint8_t ** out_data,
+    size_t * out_size,
+    const std::function<void*(size_t)>& alloc_func)
+{
+    if (out_data == nullptr || out_size == nullptr) return 1;
+    if (src == dst) {
+        *out_data = (uint8_t *)alloc_func(size);
+        if (*out_data == nullptr) return 1;
+        memcpy(*out_data, data, size);
+        *out_size = size;
+        return 0;
+    }
+
+    if ((src == RGB888 || src == BGR888) && (dst == RGB565 || dst == BGR565)) {
+        return cvt_color_888_to_565(data, size, src, dst, out_data, out_size, alloc_func);
+    }
+
+    return 1;
+}
+
+int cvt_color_888_to_565(
+    const uint8_t * data,
+    size_t size,
+    color src,
+    color dst,
+    uint8_t ** out_data,
+    size_t * out_size,
+    const std::function<void*(size_t)>& alloc_func)
+{
+    *out_size = size / 24 * 16;
+    uint16_t * allocated_data = (uint16_t *)alloc_func(*out_size);
+    if (allocated_data == nullptr) return 1;
+
+    *out_data = (uint8_t *)allocated_data;
+    size_t num_pixels = size / 3;
+    size_t r_offset = 0, b_offset = 2;
+    if (src == BGR888) {
+        r_offset = 2;
+        b_offset = 0;
+    }
+    bool target_bgr = (dst == BGR565);
+
+    if (target_bgr) {
+#pragma omp parallel for
+        for (size_t i = 0; i < num_pixels; ++i) {
+            size_t index = i * 3;
+            uint16_t r = data[index + r_offset];
+            uint16_t g = data[index + 1];
+            uint16_t b = data[index + b_offset];
+            uint16_t format565 = 0;
+            format565 = ((b & 0xF8) << 8) | ((g & 0xFC) << 3) | ((r & 0xF8) >> 3);
+            allocated_data[i] = (format565>>8) | (format565<<8);
+        }
+    } else {
+#pragma omp parallel for
+        for (size_t i = 0; i < num_pixels; ++i) {
+            size_t index = i * 3;
+            uint16_t r = data[index + r_offset];
+            uint16_t g = data[index + 1];
+            uint16_t b = data[index + b_offset];
+            uint16_t format565 = 0;
+            format565 = ((r & 0xF8) << 8) | ((g & 0xFC) << 3) | ((b & 0xF8) >> 3);
+            allocated_data[i] = (format565>>8) | (format565<<8);
+        }
+    }
+
+    return 0;
+}
 
 #endif  // __CVT_COLOR_HPP__
