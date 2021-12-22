1bdc5f76a4901ed911a8b1f5b7b1f8e3fe4e4dfa
Cocoa <89497197+cocoa-xu@users.noreply.github.com>
2021-12-22 21:16:41 +0000

    init

---
+++ lib/cvt_color.ex
@@ -0,0 +1,18 @@
+defmodule CvtColor do
+  @moduledoc """
+  Documentation for `CvtColor`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> CvtColor.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/cvt_color_test.exs
@@ -0,0 +1,8 @@
+defmodule CvtColorTest do
+  use ExUnit.Case
+  doctest CvtColor
+
+  test "greets the world" do
+    assert CvtColor.hello() == :world
+  end
+end
