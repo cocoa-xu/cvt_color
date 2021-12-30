45a64128ee981f135867a38e739e493498764aa9
Cocoa <89497197+cocoa-xu@users.noreply.github.com>
2021-12-30 01:52:33 +0000

    Update benchmark.cpp

--- c_src/benchmark.cpp
+++ c_src/benchmark.cpp
@@ -2,6 +2,8 @@
 #include <chrono>
 #include <iostream>
 #include <inttypes.h>
+#include <vector>
+#include <string>
 
 uint8_t * generate_888(size_t width, size_t height, size_t * size) {
     size_t bytes = sizeof(uint8_t) * width * height * 3;
@@ -32,21 +34,26 @@ int main() {
         uint8_t * data = generate_888(width, height, &size);
         if (data == nullptr) return -1;
 
-        auto start = std::chrono::high_resolution_clock::now();
-        for (size_t i = 0; i < num_repeat; ++i) {
-            uint8_t * out_data = nullptr;
-            size_t out_size = 0;
-            int ret = cvt_color(data, size, color::RGB888, color::RGB666Compact, &out_data, &out_size, malloc);
-            if (ret != 0) {
-                return -1;
-            } else {
-                free((void *)out_data);
+        std::vector<color> dst_colors = {color::RGB666Compact, color::RGB666, color::RGB565};
+        std::vector<std::string> dst_name = {"RGB666Compact", "RGB666", "RGB565"};
+        for (int i = 0; i < dst_colors.size(); i++) {
+            auto dst_color = dst_colors[i];
+            auto start = std::chrono::high_resolution_clock::now();
+            for (size_t i = 0; i < num_repeat; ++i) {
+                uint8_t * out_data = nullptr;
+                size_t out_size = 0;
+                int ret = cvt_color(data, size, color::RGB888, dst_color, &out_data, &out_size, malloc);
+                if (ret != 0) {
+                    return -1;
+                } else {
+                    free((void *)out_data);
+                }
             }
+            auto end = std::chrono::high_resolution_clock::now();
+            auto ns_int = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+            std::cout << "convert from RGB888 => " << dst_name[i] << ": ";
+            std::cout << ns_int.count() / 1.f / num_repeat << "ms per convert" << "\n";
         }
-        auto end = std::chrono::high_resolution_clock::now();
-        auto ns_int = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
-        std::cout << "convert from RGB888 => RGB666Compact: ";
-        std::cout << ns_int.count() / 1.f / num_repeat << "ms per convert" << "\n";
 
         free((void *)data);
     }
